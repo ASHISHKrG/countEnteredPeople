ea3cda0c03d75168e7bf6d259af5de207296f922
ASHISHKrG <102604149+ASHISHKrG@users.noreply.github.com>
2024-11-14 23:09:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+// javascript
+
+//let bonusPoints = 50
+//console.log(bonusPoints)
+//bonusPoints += 1
+//console.log(bonusPoints)
+// javascript
+let previousEntriesEl = document.getElementById("previousentries-el")
+let countEl = document.getElementById("count-el")
+console.log(countEl)
+//console.log(countEl)
+
+
+console.log(previousEntriesEl)
+let count= 0
+function increment(){
+   // console.log("Clicked")
+    count += 1
+    countEl.innerText =count
+    
+}
+
+function save(){
+    let preEntry = count + " - "
+    previousEntriesEl.innerText += preEntry
+    console.log(count)
+}
+
+
+let lap1 = 34
+let lap2 = 33
+let lap3 = 36
+
+function lapTime() {
+    let lapTime = lap1 + lap2 + lap3
+    console.log(lapTime)
+}
+
+lapTime()
+
+let lapsCompleted = 0
+
+function incrementLap() {
+    lapsCompleted += 1
+}
+
+incrementLap()
+incrementLap()
+incrementLap()
+console.log(lapsCompleted)
